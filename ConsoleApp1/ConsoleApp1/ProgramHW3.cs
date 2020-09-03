a6edb75c0fc50ed40c62ec7e12f3bdae7a28f14b
Kvazar3S273 <ba2h007@gmail.com>
2020-09-03 22:55:26 +0300

    Task1 - fihish

--- ConsoleApp1/ConsoleApp1/ProgramHW3.cs
+++ ConsoleApp1/ConsoleApp1/ProgramHW3.cs
@@ -13,16 +13,38 @@ namespace ConsoleApp1
             а також видалити частину рядка, після останнього входження цього символу і до кінця.*/
 
             string str1;
-            Console.WriteLine("Enter string");  
+            Console.WriteLine("Enter string");
             str1 = Console.ReadLine();          //ввели рядок
-            Console.WriteLine("Enter symbol");  
-            string symb;                    
+            Console.WriteLine("Enter symbol");
+            string symb;
             symb = Console.ReadLine();          //ввели символ
-            
-
+            string str2 = str1;
             str1 = str1.Replace(symb, symb.ToUpper());
-
             Console.WriteLine(str1);
+            char smb = char.Parse(symb);
+            int ind = 0;
+            for (int i = 0; i< str2.Length-1; i++)
+            {
+                if (str2[i] == smb)
+                {
+                    ind = i;
+                }
+            }
+
+            str2 = str2.Remove(ind+1);
+            str2 = str2.Replace(symb, symb.ToUpper());
+            Console.WriteLine(str2);
+
+
+            /*2   Дано два рядки.
+            Необхідно з першого рядка видалити всі символи, які зустрічаються в рядку 2.
+            Вивести одержаний рядок, а також статистику по обробці:
+            - кількість видалених символів усього
+            - кількість видалених входжень кожного символа
+            - кількість символів, яка залишилась в рядку.*/
+
+
+
         }
     }
 }
