ed4abcf41a783b0c43a361985905c0c5937abd31
Kvazar3S273 <ba2h007@gmail.com>
2020-09-20 20:45:01 +0300

    Fraction

---
+++ Fraction/Fraction/Fraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fraction
+{
+    class Fraction
+    {
+        public int up { set; get; }    //numerator (чисельник)
+        public int down { set; get; }    //denominator (знаменник)
+       
+        public static Fraction operator +(Fraction first, Fraction second)
+        {
+            Fraction f = new Fraction();
+            f.up = first.up * second.down + first.down * second.up;
+            f.down = first.down * second.down;
+            return f;
+        }
+        public static Fraction operator -(Fraction first, Fraction second)
+        {
+            Fraction f = new Fraction();
+            f.up = first.up * second.down - first.down * second.up;
+            f.down = first.down * second.down;
+            return f;
+        }
+        public static Fraction operator *(Fraction first, Fraction second)
+        {
+            Fraction f = new Fraction();
+            f.up = first.up * second.up;
+            f.down = first.down * second.down;
+            return f;
+        }
+        public static Fraction operator /(Fraction first, Fraction second)
+        {
+            Fraction f = new Fraction();
+            f.up = first.up * second.down;
+            f.down = first.down * second.up;
+            return f;
+        }
+
+
+    }
+}
---
+++ Fraction/Fraction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Fraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Введіть чисельник і через ентер знаменник");
+            Int32.TryParse(Console.ReadLine(), out num);
+            Int32.TryParse(Console.ReadLine(), out den);
+            Console.WriteLine("{0}/{1}", num, den);
+        }
+    }
+}
