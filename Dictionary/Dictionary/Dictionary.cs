1148e5885c1e22d3665c016cc3b0da1a3efe44b4
Kvazar3S273 <ba2h007@gmail.com>
2020-10-13 00:32:07 +0300

    Dictionary

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class Dictionary
+    {
+        public Dictionary<string, string> dictionary;
+
+        public Dictionary()
+        {
+            dictionary = new Dictionary<string, string>();
+        }
+        public void Add(string key, string value)
+        {
+            dictionary.Add(key, value);
+        }
+
+        public void ShowAllWordsUE()
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(" {0,10}     {1,10}", item.Key, item.Value);
+            }
+
+        }
+        public void ShowAllWordsEU()
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(" {0,10}     {1,10}", item.Value, item.Key);
+            }
+
+        }
+        public bool TranslateUE(string word)
+        {
+            if (dictionary.ContainsKey(word) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Слово відсутнє");
+                Console.ResetColor();
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("{0}  -  {1}", word, dictionary[word]);
+                return true;
+            }
+        }
+        public bool TranslateEU(string word)
+        {
+            if (dictionary.ContainsValue(word) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Слово відсутнє");
+                Console.ResetColor();
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("{0}  -  {1}", dictionary[word], word);
+                return true;
+            }
+        }
+
+        //public string TranslateUE(string word)
+        //{
+        //    string result="";
+        //    foreach (var item in dictionary.Keys)
+        //    {
+        //        if((string)dictionary[item]==word)
+        //        {
+        //            result=(string)item;
+        //        }
+        //    }
+        //        return result;
+        //}
+        //public string TranslateEU(string word)
+        //{
+        //    string result = "";
+        //    foreach (var item in dictionary.Keys)
+        //    {
+        //        if ((string)item == word)
+        //        {
+        //            result = (string)dictionary[item];
+        //        }
+        //    }
+        //    return result;
+        //}
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            Dictionary dictionary = new Dictionary();
+            dictionary.Add("Україна", "Ukraine");
+            dictionary.Add("Польща", "Poland");
+            dictionary.Add("Словаччина", "Slovakia");
+            dictionary.Add("Румунія", "Romania");
+            dictionary.Add("Угорщина", "Hungary");
+            dictionary.Add("Німечччина", "Germany");
+            dictionary.Add("Франція", "France");
+            dictionary.Add("Нідерланди", "Holland");
+            dictionary.Add("Бельгія", "Belgium");
+            dictionary.Add("Білорусь", "Bieloruss");
+            dictionary.Add("Туреччина", "Turkey");
+            dictionary.Add("Канада", "Canada");
+            dictionary.Add("Мексика", "Mexico");
+            dictionary.Add("США", "USA");
+
+
+            while (true)                        
+            {
+                Console.Clear();
+                int counter = 1;
+                ConsoleKey key = ConsoleKey.Escape;
+                do
+                {
+                    Console.Clear();
+                    if (counter == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("\n\n\n\n\t* Українсько-англійський словник");
+                    Console.ResetColor();
+                    if (counter == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("\t* Англо-український словник");
+                    Console.ResetColor();
+                    
+                    if (counter == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine("\t* Вихід");
+                    Console.ResetColor();
+
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    key = keyInfo.Key;
+
+                    switch (key)    //обхід меню стрілочками
+                    {
+                        case (ConsoleKey)ConsoleKey.DownArrow:
+                            {
+                                if (counter < 3) { counter++; }
+                                else { counter = 1; }
+                                break;
+                            }
+                        case (ConsoleKey)ConsoleKey.UpArrow:
+                            {
+                                if (counter > 1) { counter--; }
+                                else { counter = 3; }
+                                break;
+                            }
+                    }
+                } while (key != ConsoleKey.Enter);
+                Console.Clear();
+                switch (counter)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Зробіть вибір:");
+                            Console.WriteLine("(1) - Переглянути всі слова");
+                            Console.WriteLine("(2) - Здійснити переклад з української на англійську");
+                            int tempKey = int.Parse(Console.ReadLine());
+                            switch(tempKey)
+                            {
+                                case 1:
+                                    {
+                                        dictionary.ShowAllWordsUE();
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Console.WriteLine("Введіть назву країни УКР мовою:");
+                                        string countryU = Console.ReadLine();
+                                        dictionary.TranslateUE(countryU);
+                                        break;
+                                    }
+                            }
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Зробіть вибір:");
+                            Console.WriteLine("(1) - Переглянути всі слова");
+                            Console.WriteLine("(2) - Здійснити переклад з англійської на українську");
+                            int tempKey = int.Parse(Console.ReadLine());
+                            switch (tempKey)
+                            {
+                                case 1:
+                                    {
+                                        dictionary.ShowAllWordsEU();
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        Console.WriteLine("Введіть назву країни АНГЛ мовою:");
+                                        string countryE = Console.ReadLine();
+                                        dictionary.TranslateEU(countryE);
+                                        break;
+                                    }
+                            }
+
+
+                            //Console.ForegroundColor = ConsoleColor.Cyan;
+
+                            //Console.ResetColor();
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 3:
+                        {
+                            return;
+                            break;
+                        }
+                }
+                
+            }
+
+
+
+
+        }
+    }
+}
