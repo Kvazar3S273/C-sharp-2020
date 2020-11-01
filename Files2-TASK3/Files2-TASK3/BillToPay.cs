09ae80f491061e3d046dc0bc71464ee58c2a9c3d
Kvazar3S273 <ba2h007@gmail.com>
2020-11-01 23:57:19 +0200

    TASK3

---
+++ Files2-TASK3/Files2-TASK3/BillToPay.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Files2_TASK3
+{
+    [Serializable]
+    class BillToPay
+    {
+        public static bool FlagSerialize = true;      // Мітка серіалізації
+        
+        [XmlElement]
+        public string PaymentForDay { get; set; }       // Оплата за 1 день
+        
+        [XmlElement]
+        public string FullDayCount { get; set; }        // Кількість днів
+        
+        [XmlElement]
+        public string FineForDay { get; set; }          // Штраф за 1 день протермінування
+        
+        [XmlElement]
+        public string FineDayCount { get; set; }        // Кількість протермінованих днів
+
+        [XmlElement]
+        public string SumPaymentNoFine { get; set; }    // Сума до оплати без штрафа
+        
+        [XmlElement]
+        public string SumFine { get; set; }             // Сума штрафа
+        
+        [XmlElement]
+        public string FullPaymentSum { get; set; }      // Загальна сума до оплати
+
+        public BillToPay()
+        {
+        }
+
+        public BillToPay(string paymentForDay, string fullDayCount, string fineForDay, string fineDayCount)
+        {
+            PaymentForDay = paymentForDay;
+            FullDayCount = fullDayCount;
+            FineForDay = fineForDay;
+            FineDayCount = fineDayCount;
+
+            SumPaymentNoFine = (int.Parse(paymentForDay) * int.Parse(fullDayCount)).ToString();
+            SumFine = (int.Parse(fineForDay) * int.Parse(fineDayCount)).ToString();
+            FullPaymentSum = (int.Parse(SumPaymentNoFine) + int.Parse(SumFine)).ToString();
+        }
+        public override string ToString()
+        {
+            return "\nСума до оплати без штрафа " + SumPaymentNoFine 
+                + "\nСума штрафа " + SumFine + "\nЗагальна сума до оплати " + FullPaymentSum;
+        }
+
+        //[OnSerializing]
+        //public void ToSerializeWhenFalse()
+        //{
+        //    SumPaymentNoFine = SumPaymentNoFine.Replace(SumPaymentNoFine,"-----------------");
+        //    SumFine = SumFine.Replace(SumFine, "-----------------");
+        //    FullPaymentSum = FullPaymentSum.Replace(FullPaymentSum, "-----------------");
+        //}
+
+        //[OnDeserialized]
+        //public void ToDeserializeWhenFalse()
+        //{
+        //    this.SumPaymentNoFine = SumPaymentNoFine;
+        //    this.SumFine = SumFine;
+        //    this.FullPaymentSum = FullPaymentSum;
+        //}
+
+
+
+
+    }
+}
---
+++ Files2-TASK3/Files2-TASK3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Files2_TASK3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            // Створюємо рахунок перший раз і серіалізуємо його в файл "j3.json"
+            // -----------------------------------------------------------------
+            //BillToPay btp = new BillToPay("400", "7", "140", "4");
+            //btp.ToSerializeWhenFalse();
+            //string jsonString;
+            //jsonString = JsonSerializer.Serialize(btp);//, options);
+            //File.WriteAllText(@"g:\step\jjj.json", jsonString);
+            // ------------------------------------------------------------------
+
+            //// Десеріалізація--------------------------------------------------
+            //string jsonString = File.ReadAllText(@"g:\step\jjj.json");
+            //BillToPay btp = JsonSerializer.Deserialize<BillToPay>(jsonString);
+            //Console.WriteLine(btp);
+            ////-----------------------------------------------------------------
+
+            BillToPay btp = new BillToPay("400", "7", "140", "4");
+
+            BillToPay.FlagSerialize = true;
+            using (FileStream fs = new FileStream(@"g:\step\my.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                XmlSerializer format = new XmlSerializer(typeof(BillToPay));
+                format.Serialize(fs, btp);
+            }
+
+
+            Console.WriteLine("Якщо ви хочете серіалізувати весь список-натисніть +");
+            Console.WriteLine("Якщо ви хочете серіалізувати не весь список-натисніть *");
+            ConsoleKeyInfo keys = Console.ReadKey();
+            if (keys.KeyChar == '+')
+            {
+
+                //Серіалізуємо все.
+
+                if (BillToPay.FlagSerialize)
+                {
+
+                    string path1 = @"g:\step\my.xml";
+                    var serializer = new XmlSerializer(typeof(BillToPay));
+
+                    using (var stream = File.OpenWrite(path1))
+                    {
+                        serializer.Serialize(stream, btp);
+                    }
+                }
+            }
+            if (keys.KeyChar == '*')
+            {
+                BillToPay.FlagSerialize = false;
+
+                // Серіалізуємо тільки певні поля.
+
+
+                if (!BillToPay.FlagSerialize)
+                {
+                    using (XmlWriter writer = XmlWriter.Create(@"g:\step\my.xml"))
+                    {
+
+
+                        writer.WriteStartDocument();
+
+                            writer.WriteStartElement("Account");
+                            writer.WriteElementString("TotalSum", btp.SumPaymentNoFine);
+                            writer.WriteElementString("PenaltySum", btp.SumFine);
+                            writer.WriteElementString("FinishSum", btp.FullPaymentSum);
+
+
+                            writer.WriteEndElement();
+
+                        writer.WriteEndDocument();
+                    }
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
