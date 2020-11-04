433e0bb671728a356fcd194dfce8f9e43d1bce7f
Kvazar3S273 <ba2h007@gmail.com>
2020-11-04 23:08:14 +0200

    Norton Commander start

---
+++ NortonCommander/NortonCommander/Drives.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NortonCommander
+{
+    public class Drives
+    {
+        public void ShowDrive(string name)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            //for (int i = 0; i < drives.Length; i++)
+            //{
+            //    Console.WriteLine($"Назва диска: {drives[i].Name}");
+            //    Console.WriteLine($"Тип: {drives[i].DriveType}");
+            //    if (drives[i].IsReady)
+            //    {
+            //        Console.WriteLine($"Об\'єм диска: {drives[i].TotalSize / 1024 / 1024 / 1024} Gb");
+            //        Console.WriteLine($"Доступно мiсця: {drives[i].TotalFreeSpace / 1024 / 1024 / 1024} Gb");
+            //    }
+            //    Console.WriteLine();
+            //}
+            int discNumber = 0;
+            switch (name)
+            {
+                case "c":
+                case "C":
+                    discNumber = 0;
+                    break;
+                case "d":
+                case "D":
+                    discNumber = 1;
+                    break;
+                case "e":
+                case "E":
+                    discNumber = 2;
+                    break;
+                case "f":
+                case "F":
+                    discNumber = 3;
+                    break;
+                case "g":
+                case "G":
+                    discNumber = 4;
+                    break;
+                //case "h":
+                //case "H":
+                //    discNumber = 5;
+                //    break;
+                //case "i":
+                //case "I":
+                //    discNumber = 6;
+                //    break;
+                default:
+                    Console.WriteLine("Wrong choice!");
+                    break;
+            }
+
+            Console.WriteLine($"Назва диска: {drives[discNumber].Name}");
+            Console.WriteLine($"Тип: {drives[discNumber].DriveType}");
+            if (drives[discNumber].IsReady)
+            {
+                Console.WriteLine($"Об\'єм диска: {drives[discNumber].TotalSize / 1024 / 1024 / 1024} Gb");
+                Console.WriteLine($"Доступно мiсця: {drives[discNumber].TotalFreeSpace / 1024 / 1024 / 1024} Gb");
+            }
+        }
+       
+    }
+}
---
+++ NortonCommander/NortonCommander/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace NortonCommander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drives disc = new Drives();
+            Console.WriteLine("Choice disc: A-B-C-D-E-F-G-H-I");
+            string discName = Console.ReadLine();
+            disc.ShowDrive(discName);
+
+            string dirName = discName+":\\";
+            Console.WriteLine();
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Пiдкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файли:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+    }
+}
