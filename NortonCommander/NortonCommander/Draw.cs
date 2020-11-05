3806245c9b2fbb3f3df56b20f477a7a32162cc5b
Kvazar3S273 <ba2h007@gmail.com>
2020-11-05 23:17:49 +0200

    Draw table

---
+++ NortonCommander/NortonCommander/Draw.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NortonCommander
+{
+    class Draw
+    {
+        public void Title()
+        {
+            Console.WriteLine("\n   The Total Commander, Version 1.00 Copyright (C) 2020 by Volodymyr Blonsky\n");
+        }
+        public void DrawOnceTable(int startX, int startY)
+        {
+            // 201 ╔        187  ╗
+            // 205 ════ (4) 
+            // 205 ═══  (3)
+            // 205 ════════════  (12)
+            // 186 ║    206 ╬     204 ╠      185 ╣  
+            // 200 ╚    188 ╝
+            // 209 ╤    207 ╧     199 ╟      182 ╢
+            // 193 ┴    179 │     
+            // 196 ──── (4)
+            // 196 ─── (3)
+            // 196 ──────────── (12)
+
+            Console.SetCursorPosition(startX, startY);
+            Console.WriteLine("╔════════════╤════════════╤════════════╗");
+            for (int i = 1; i < 20; i++)
+            {
+                Console.SetCursorPosition(startX, startY + i);
+                Console.WriteLine("║            │            │            ║");
+            }
+            Console.SetCursorPosition(startX, startY + 20);
+            Console.WriteLine("╟────────────┴────────────┴────────────╢");
+            Console.SetCursorPosition(startX, startY + 21);
+            Console.WriteLine("║                                      ║");
+            Console.SetCursorPosition(startX, startY + 22);
+            Console.WriteLine("╚══════════════════════════════════════╝");
+        }
+        public void DrawFooter()
+        {
+            Console.SetCursorPosition(0, 27);
+            Console.ResetColor();
+            Console.Write("1");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Help  ");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("2");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Menu  ");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("3");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("View  ");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("4");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Edit  ");
+            Console.ResetColor();
+            Console.Write(" "); 
+            Console.Write("5");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Copy  ");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("6");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("RenMov");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("7");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Mkdir ");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("8");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Delete");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("9");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("PullDn");
+            Console.ResetColor();
+            Console.Write(" ");
+            Console.Write("10");
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Quit  ");
+            Console.ResetColor();
+            Console.Write(" ");
+        }
+
+        public void DrawDiscChoise(int startX, int startY)
+        {
+            Console.SetCursorPosition(startX, startY);
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("                                      ");
+            Console.SetCursorPosition(startX, startY + 1);
+            Console.Write("   ╔════════ ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("DRIVE LETTER");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write(" ════════╗   ");
+            Console.SetCursorPosition(startX, startY + 2);
+            Console.WriteLine("   ║         Choose drive:        ║   ");
+            Console.SetCursorPosition(startX, startY + 3);
+            Console.WriteLine("   ║   C   D   E   F   G   H   I  ║   "); 
+            Console.SetCursorPosition(startX, startY + 4);
+            Console.WriteLine("   ╚══════════════════════════════╝   ");
+            Console.SetCursorPosition(startX, startY + 5);
+            Console.WriteLine("                                      ");
+
+            Console.ResetColor();
+
+        }
+    }
+}
--- NortonCommander/NortonCommander/Program.cs
+++ NortonCommander/NortonCommander/Program.cs
@@ -1,36 +1,61 @@
 ﻿using System;
 using System.IO;
-
+using System.Runtime.InteropServices;
+            
+// Консоль 150х60
+// 80x25
 namespace NortonCommander
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Drives disc = new Drives();
-            Console.WriteLine("Choice disc: A-B-C-D-E-F-G-H-I");
-            string discName = Console.ReadLine();
-            disc.ShowDrive(discName);
 
-            string dirName = discName+":\\";
+
+            Draw title = new Draw();
+            title.Title();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Draw tableLeft = new Draw();
+            Draw tableRight = new Draw();
+
+            
+            tableLeft.DrawOnceTable(0, 3);
+            tableRight.DrawOnceTable(40, 3);
+            
+            Draw discChoice = new Draw();
+            discChoice.DrawDiscChoise(1, 7);
+
+            Console.ResetColor();
+            Draw footer = new Draw();
+            footer.DrawFooter();
+
+            // Вивід інформації про диск
+            //Drives disc = new Drives();
+            //Console.WriteLine("Choice disc: A-B-C-D-E-F-G-H-I");
+            //string discName = Console.ReadLine();
+            //disc.ShowDrive(discName);
+
+
+            //string dirName = discName+":\\";
+            //Console.WriteLine();
+            //if (Directory.Exists(dirName))
+            //{
+            //    Console.WriteLine("Пiдкаталоги:");
+            //    string[] dirs = Directory.GetDirectories(dirName);
+            //    foreach (string s in dirs)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //    Console.WriteLine();
+            //    Console.WriteLine("Файли:");
+            //    string[] files = Directory.GetFiles(dirName);
+            //    foreach (string s in files)
+            //    {
+            //        Console.WriteLine(s);
+            //    }
+            //}
             Console.WriteLine();
-            if (Directory.Exists(dirName))
-            {
-                Console.WriteLine("Пiдкаталоги:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
-                {
-                    Console.WriteLine(s);
-                }
-                Console.WriteLine();
-                Console.WriteLine("Файли:");
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    Console.WriteLine(s);
-                }
-            }
-
         }
     }
 }
